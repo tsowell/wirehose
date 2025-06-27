0a7f22fdc3185417c0cf5f093305143c16e9c48f
Thomas Sowell <tom@ldtlb.com>
2025-06-26 18:46:12 -0800

    Move Command into monitor module

---
+++ src/monitor/command.rs
@@ -0,0 +1,16 @@
+//! PipeWire controls which can be executed by the monitor module.
+
+use crate::monitor::ObjectId;
+
+#[derive(Debug)]
+pub enum Command {
+    NodeMute(ObjectId, bool),
+    DeviceMute(ObjectId, i32, i32, bool),
+    NodeVolumes(ObjectId, Vec<f32>),
+    DeviceVolumes(ObjectId, i32, i32, Vec<f32>),
+    DeviceSetRoute(ObjectId, i32, i32),
+    DeviceSetProfile(ObjectId, i32),
+    NodeCaptureStart(ObjectId, u64, bool),
+    NodeCaptureStop(ObjectId),
+    MetadataSetProperty(ObjectId, u32, String, Option<String>, Option<String>),
+}
